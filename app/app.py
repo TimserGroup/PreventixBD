093e86fab0f17638d8e2c204ec72767a5b83d7a1
RodolfoHP1 <rodolfohp@timsergroup.com>
2023-06-08 01:00:45 -0600

    App

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'sql9624432'
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'BNMN2TSHZL'
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'sql9.freemysqlhosting.net'
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'sql9624432'
-app.config['MYSQL_PORT'] = int(os.getenv('PORT')) or 3306
+app.config['MYSQL_PORT'] = os.getenv('PORT') or 3306
 
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
