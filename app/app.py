fa0e7c722851a4d54b3f316f64cfcb2f32b04c80
TimserGroup <135937461+TimserGroup@users.noreply.github.com>
2023-06-07 22:17:16 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,7 @@
 from flask import Flask
 
-from pacientes import pacientes
-import pandas as pd
-
 # Application initializations
 app = Flask(__name__)
 
 # settings
 app.secret_key = "mysecretkey"
-
-app.register_blueprint(pacientes)
-
-# starting the app
-if __name__ == "__main__":
-    app.run(port=5000, debug=False)
