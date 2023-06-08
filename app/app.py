9c88e0975e43420481370e4e364911d615d9b7ca
TimserGroup <135937461+TimserGroup@users.noreply.github.com>
2023-06-07 22:13:36 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,12 @@ from flask import Flask
 from pacientes import pacientes
 import pandas as pd
 
+# Application initializations
+app = Flask(__name__)
+
+# settings
+app.secret_key = "mysecretkey"
+
 app.register_blueprint(pacientes)
 
 # starting the app
