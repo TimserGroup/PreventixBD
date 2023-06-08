5f3f06e4b786dd037bbdfdd494d655f6053bafd1
RodolfoHP1 <rodolfohp@timsergroup.com>
2023-06-07 23:46:02 -0600

    App

--- app/app.py
+++ app/app.py
@@ -1,13 +1,15 @@
-from flask import Blueprint, request, render_template, redirect, url_for, flash, app, Flask
-
+from flask import Blueprint, request, render_template, redirect, url_for, flash, Flask
 import numpy as np
 from scipy.stats import chi2_contingency
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
+
 # Mysql Settings
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'sql9624432'
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'BNMN2TSHZL'
@@ -18,25 +20,16 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # MySQL Connection
 mysql = MySQL(app)
-# Application initializations
 
-
-# settings
-app.secret_key = "mysecretkey"
-
-
-
-pacientes = Blueprint('pacientes', __name__,template_folder='app/templates')
-
+pacientes = Blueprint('pacientes', __name__, template_folder='app/templates')
 
 @app.route('/')
-def Index():
+def index():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM pacientes')
     data = cur.fetchall()
     cur.close()
     return render_template('index.html', pacientes=data)
-
 
 @app.route('/add_paciente', methods=['POST'])
 def add_paciente():
